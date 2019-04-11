cd4d8c26f17b261351f21db47a51b07de00ae38f
YutaNagaoka <shirasu.okatin@gmail.com>
2019-04-11 23:14:24 +0900

    feat: implemented tokenizer.

--- 9cc.c
+++ 9cc.c
@@ -1,5 +1,52 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+enum {
+	TK_NUM = 256,
+	TK_EOF,
+};
+
+typedef struct {
+	int type;
+	int value;
+	char *input;
+} Token;
+
+Token tokens[100];
+
+void tokenize(char *p) {
+	int i = 0;
+	while (*p) {
+		if(isspace(*p)) {
+			p++;
+		}
+		else if (*p == '+' || *p == '-') {
+			tokens[i].type = *p;
+			tokens[i].input = p;
+			i++;
+			p++;
+		}
+		else if (isdigit(*p)) {
+			tokens[i].type = TK_NUM;
+			tokens[i].input = p;
+			tokens[i].value = strtol(p, &p, 10);
+			i++;
+		}
+		else {
+			fprintf(stderr, "Cannot tokenize: %s\n", p);
+			exit(1);
+		}
+	}
+	tokens[i].type = TK_EOF;
+	tokens[i].input = p;
+}
+
+void error(int i) {
+	fprintf(stderr, "Unexpected token: %s\n", tokens[i].input);
+	exit(1);
+}
 
 int main(int argc, char **argv){
 	if (argc != 2){
@@ -7,23 +54,37 @@ int main(int argc, char **argv){
 		return 1;
 	}
 
-	char *p = argv[1];
+	tokenize(argv[1]);
+
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
 	printf("main:\n");
-	printf("  mov rax, %ld\n", strtol(p, &p, 10));
-	while (*p){
-		if (*p == '+'){
-			p++;
-			printf("  add rax, %ld\n", strtol(p, &p, 10));
+
+	if (tokens[0].type != TK_NUM) {
+		error(0);
+	}
+	printf("  mov rax, %d\n", tokens[0].value);
+
+	int i = 1;
+	while (tokens[i].type != TK_EOF) {
+		if (tokens[i].type == '+') {
+			i++;
+	        if (tokens[i].type != TK_NUM) {
+		        error(i);
+	        }
+			printf("  add rax, %d\n", tokens[i].value);
+			i++;
 		}
-		else if (*p == '-'){
-			p++;
-			printf("  sub rax, %ld\n", strtol(p, &p, 10));
+		else if (tokens[i].type == '-') {
+			i++;
+	        if (tokens[i].type != TK_NUM) {
+		        error(i);
+	        }
+			printf("  sub rax, %d\n", tokens[i].value);
+			i++;
 		}
 		else {
-			fprintf(stderr, "Unexpected charactor: '%c'\n", *p);
-			return 1;
+			error(i);
 		}
 	}
 	printf("  ret\n");
--- test.sh
+++ test.sh
@@ -19,4 +19,5 @@ try() {
 try 0 0
 try 42 42
 try 21 '5+20-4'
+try 44 "12 + 34 - 5 + 3"
 echo OK
