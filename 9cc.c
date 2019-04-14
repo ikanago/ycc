fb5da8551395ca706560e816eeb79eb8484dae22
YutaNagaoka <shirasu.okatin@gmail.com>
2019-04-14 22:12:26 +0900

    feat: implemented multiple, division, and calculation with parentheses.

--- 9cc.c
+++ 9cc.c
@@ -3,10 +3,49 @@
 #include <stdlib.h>
 #include <string.h>
 
+int pos = 0;
+
+typedef struct Node {
+	int type;
+	int value;
+	struct Node *lhs;
+	struct Node *rhs;
+} Node;
+
+Node *new_node(int, Node*, Node*);
+Node *new_node_num(int);
+int consume(int);
+void error(int);
+Node *term();
+Node *mul();
+Node *add();
+void gen(Node*);
+void tokenize(char*);
+
 enum {
 	TK_NUM = 256,
 	TK_EOF,
 };
+
+enum {
+	ND_NUM = 256,
+};
+
+
+Node *new_node(int type, Node *lhs, Node *rhs) {
+	Node *node = malloc(sizeof(Node));
+	node->type = type;
+	node->lhs = lhs;
+	node->rhs = rhs;
+	return node;
+}
+
+Node *new_node_num(int value) {
+	Node *node = malloc(sizeof(Node));
+	node->type = ND_NUM;
+	node->value = value;
+	return node;
+}
 
 typedef struct {
 	int type;
@@ -16,13 +55,96 @@ typedef struct {
 
 Token tokens[100];
 
+int consume(int type) {
+	if (tokens[pos].type != type)
+		return 0;
+	pos++;
+	return 1;
+}
+
+void error(int i) {
+	fprintf(stderr, "Unexpected token: %s\n", tokens[i].input);
+	exit(1);
+}
+
+Node *term() {
+	if (consume('(')) {
+		Node *node = add();
+		if(!consume(')'))
+			printf("There is no close parenthese corresponds open one: %s",
+					tokens[pos].input);
+		return node;
+	}
+
+	if (tokens[pos].type == TK_NUM)
+		return new_node_num(tokens[pos++].value);
+
+    printf("This is token which is neither value nor open parenthese: %s",
+			tokens[pos].input);
+}
+
+Node *mul() {
+	Node *node = term();
+    for(;;) {
+		if (consume('*'))
+			node = new_node('*', node, mul());
+		else if (consume('/'))
+			node = new_node('/', node, mul());
+		else
+			return node;
+	}
+}
+
+Node *add() {
+	Node *node = mul();
+
+	for(;;) {
+		if (consume('+'))
+			node = new_node('+', node, mul());
+		else if (consume('-'))
+			node = new_node('-', node, mul());
+		else
+			return node;
+	}
+}
+
+void gen(Node *node) {
+	if (node->type == ND_NUM) {
+		printf("  push %d\n", node->value);
+		return;
+	}
+
+	gen(node->lhs);
+	gen(node->rhs);
+
+	printf("  pop rdi\n");
+	printf("  pop rax\n");
+
+	switch (node->type){
+		case '+':
+			printf("  add rax, rdi\n");
+			break;
+		case '-':
+			printf("  sub rax, rdi\n");
+			break;
+		case '*':
+			printf("  mul rdi\n");
+			break;
+		case '/':
+			printf("  mov rdx, 0\n");
+			printf("  div rdi\n");
+	}
+
+	printf("  push rax\n");
+}
+
 void tokenize(char *p) {
 	int i = 0;
 	while (*p) {
 		if(isspace(*p)) {
 			p++;
 		}
-		else if (*p == '+' || *p == '-') {
+		else if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')') {
 			tokens[i].type = *p;
 			tokens[i].input = p;
 			i++;
@@ -43,11 +165,6 @@ void tokenize(char *p) {
 	tokens[i].input = p;
 }
 
-void error(int i) {
-	fprintf(stderr, "Unexpected token: %s\n", tokens[i].input);
-	exit(1);
-}
-
 int main(int argc, char **argv){
 	if (argc != 2){
 		fprintf(stderr, "The number of arguments is incorrect.\n");
@@ -55,38 +172,15 @@ int main(int argc, char **argv){
 	}
 
 	tokenize(argv[1]);
+	Node *node = add();
 
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
 	printf("main:\n");
 
-	if (tokens[0].type != TK_NUM) {
-		error(0);
-	}
-	printf("  mov rax, %d\n", tokens[0].value);
+	gen(node);
 
-	int i = 1;
-	while (tokens[i].type != TK_EOF) {
-		if (tokens[i].type == '+') {
-			i++;
-	        if (tokens[i].type != TK_NUM) {
-		        error(i);
-	        }
-			printf("  add rax, %d\n", tokens[i].value);
-			i++;
-		}
-		else if (tokens[i].type == '-') {
-			i++;
-	        if (tokens[i].type != TK_NUM) {
-		        error(i);
-	        }
-			printf("  sub rax, %d\n", tokens[i].value);
-			i++;
-		}
-		else {
-			error(i);
-		}
-	}
+	printf("  pop rax\n");
 	printf("  ret\n");
 	return 0;
 }
